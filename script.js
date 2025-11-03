d02d5d3399b879b7f5d73fffcc4e3c54e996c71b
Surjeet Kumar <157746293+surjeetkumar8006@users.noreply.github.com>
2025-11-03 11:53:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-//your JS code here. If required.
+function mergeCraftDates(emily, oliver) {
+  let merged = [];
+  let i = 0, j = 0;
+
+  while (i < emily.length && j < oliver.length) {
+    if (emily[i] < oliver[j]) {
+      merged.push(emily[i]);
+      i++;
+    } else {
+      merged.push(oliver[j]);
+      j++;
+    }
+  }
+
+  while (i < emily.length) merged.push(emily[i++]);
+  while (j < oliver.length) merged.push(oliver[j++]);
+
+  return merged;
+}
+
+document.getElementById('mergeBtn').addEventListener('click', function() {
+  const emily = [3, 7, 12, 20];
+  const oliver = [1, 5, 8, 18, 25];
+  const result = mergeCraftDates(emily, oliver);
+  document.getElementById('output').innerText = "Merged Record: " + result.join(', ');
+});
